057932b9004f696f5afc5f25cc0a648d6e20a9cd
CarlosEnoc <enoc.calzada@gmai.com>
2024-11-12 14:00:30 -0600

    204

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Categoria;
 use App\Models\Salario;
+use App\Models\Vacante;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -41,6 +42,33 @@ class CrearVacante extends Component
 public function crearVacante(){
 
     $datos=$this->validate();
+
+    //Almacenar la Imagen 
+    //$this->imagen->store('public/vacantes');
+    $imagen = $this->imagen->store('public/vacantes');
+
+    $datos['imagen']=str_replace('public/vacantes/','',$imagen);
+
+    //dd($nombre_imagen);
+
+    //Crear la Vacante
+    Vacante::create([
+        'titulo'=>  $datos['titulo'],
+        'salario_id'=> $datos['salario'],
+        'categoria_id'=> $datos['categoria'],
+        'empresa'=> $datos['empresa'],
+        'ultimo_dia'=> $datos['ultimo_dia'],
+        'descripcion'=> $datos['descripcion'],
+        'imagen'=> $datos['imagen'],
+        'user_id'=> auth()->user()->id,
+
+    ]);
+
+    //Crear Mensaje
+    session()->flash('mensaje','La Vacante se publico Correctamente');
+
+    //Redireccionar al usuario
+    return redirect()->route('vacantes.index');
 
 
 }
--- app/Models/Vacante.php
+++ app/Models/Vacante.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vacante extends Model
 {
     //
+    use HasFactory;
+
+    protected $fillable=[
+        'titulo',
+        'salario_id',
+        'categoria_id',
+        'empresa',
+        'ultimo_dia',
+        'descripcion',
+        'imagen',
+        'user_id'
+    ];
 }
