bb116b5631835c8b3187ca9dfacae87888652946
CarlosEnoc <enoc.calzada@gmai.com>
2024-11-11 23:35:48 -0600

    197 commit

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -5,9 +5,46 @@ namespace App\Livewire;
 use App\Models\Categoria;
 use App\Models\Salario;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CrearVacante extends Component
+
 {
+
+    public $titulo;
+    public $salario;
+
+    public $categoria;
+
+    public $empresa;
+
+    public $ultimo_dia;
+
+    public $descripcion;
+
+    public $imagen;
+
+    use WithFileUploads;
+
+    protected $rules=[
+        'titulo'=>'required|string',
+        'salario'=>'required',
+        'categoria'=>'required',
+        'empresa'=>'required',
+        'ultimo_dia'=>'required',
+        'descripcion'=>'required',
+        'imagen'=>'required|image|max:1024',
+
+    ];
+
+
+public function crearVacante(){
+
+    $datos=$this->validate();
+
+
+}
+
     public function render()
     {
 
